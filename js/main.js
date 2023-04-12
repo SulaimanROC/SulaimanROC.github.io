f62e0a75a765a3e6902805b7721b533c8094e9b0
Sulaiman Hussain <302417676@student.rocmondriaan.nl>
2023-04-12 14:54:33 +0200

    profiel pagna bijna af

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-const profielkenmerken = {
-    EmailAdres: 'Sulaiman@gmail.com
+const profielKenmerken = {
+    EmailAdress: 'Sulaiman@gmail.com',
+    UserName: 'SulaimanROC',
+    BirthDate: '2005 27 september',
+    ProfilePicture: 'empty',
+    Biografy: 'I live in the Hague and go to ROC mondriaan following the software development study.',
+    Projects: 'Hoger lager, Boter Kaas eieren, web catalogus',
+    Study: 'Software Development',
+    Abilities: 'Coding, reading and fluent in english',
+    BannerColor: 'black',
+    SocialMedia: 'Instagram: Sulaiman, Twitter: Sulaiman123'
+}
+
+for (key in profielKenmerken) {
+    console.log(key); //name, occupation, ship
+    console.log(profielKenmerken[key]); // Jean Luc, Captain, USS Enterprise
 }
